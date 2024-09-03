131351ebf85b107c411da50403928e92db83511e
ashwinii-gv <ashwinigv237@gmail.com>
2024-09-03 10:03:13 +0530

    First commit

---
+++ coreJava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,14 @@
+//Program to demonstrate encapsulation
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNum;//data variables
+	String name;
+	int age;
+	
+	void show()//member function
+	{
+		System.out.println("searialnumber"+serialNum+"\t Name"+name +"\t Age"+ age);
+	}
+
+}
---
+++ coreJava/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation ob= new Encapsulation();//creating an object
+		ob.name="Aswhini";
+		ob.age=21;
+		ob.serialNum=22;
+		ob.show();  //by using ob we can call methods
+
+	}
+
+}
